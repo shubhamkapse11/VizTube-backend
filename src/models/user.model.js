164f5b4d95f575d135f2fc13d3273c848398e5bf
shubhamkapse11 <shubhamkapse1110@gmail.com>
2026-01-01 23:47:21 +0530

    finsl mommit project done

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -62,36 +62,36 @@ userSchema.pre("save", async function (next) {
 
 userSchema.methods.isPasswordCorrect = async function (password) {
   const res = await bcrypt.compare(password, this.password);
-  return res ;
+  return res;
 };
 
-userSchema.methods.generateAccessToken = function (){
-   return  jwt.sign({
-        _id : this._id ,
-        email:this.email,
-        username : this.username,
-        fullname : this.fullname,
-    },
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign({
+    _id: this._id,
+    email: this.email,
+    username: this.username,
+    fullname: this.fullname,
+  },
     process.env.ACCESS_TOKEN_SECRET,
     {
-       expiresIn : process.env.ACCESS_TOKEN_EXPIRY 
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY
     }
-)
- }
+  )
+}
 
-userSchema.methods.generateRefreshToken = function (){
-     const token =   jwt.sign({
-        _id : this._id ,
-        // email:this.email,
-        // username : this.username,
-        // fullname : this.fullname,
-    },
+userSchema.methods.generateRefreshToken = function () {
+  const token = jwt.sign({
+    _id: this._id,
+    // email:this.email,
+    // username : this.username,
+    // fullname : this.fullname,
+  },
     process.env.REFRESH_TOKEN_SECRET,
     {
-       expiresIn : process.env.REFRESH_TOCKEN_EXPIRY 
+      expiresIn: process.env.REFRESH_TOCKEN_EXPIRY
     }
-)
-return token ;
+  )
+  return token;
 }
 
 
